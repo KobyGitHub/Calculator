756243aa31f1543527e8a0182f0e4c46a4dfd47e
KobyGitHub <kayzjean@gmail.com>
2022-10-10 20:42:38 -0500

    Test

--- calculator-test.js
+++ calculator-test.js
@@ -1,12 +1,29 @@
 
 it('should calculate the monthly rate correctly', function () {
-  const accValues = getCurrentUIValues();
-  expect(calculateMonthlyPayment(accValues)).toEqual('130.44');
+  const values = {
+    amount: 45000,
+    years: 55,
+    rate: 8
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('303.78');
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  const values = {
+    amount: 10043,
+    years: 8,
+    rate: 5.8
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('131.00');
 });
 
-/// etc
+it("should handle terribly high interest rates", function() {
+  const values = {
+    amount: 1000,
+    years: 40,
+    rate: 99
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('82.50');
+});
+
--- calculator.js
+++ calculator.js
@@ -1,13 +1,71 @@
+// window.addEventListener('DOMContentLoaded', function() {
+//   const form = document.getElementById("calc-form");
+  
+//   if (form) {
+//     setupIntialValues();
+//     form.addEventListener("submit", function(e) {
+//       e.preventDefault();
+//       update();
+//     });
+//   }
+// });
+
+
+// //const cal = document.getElementById("calc-submit") 
+
+
+
+// function getCurrentUIValues() {
+//   const loanAmount = document.getElementById("loan-amount")
+//   const loanYears = document.getElementById("loan-years")
+//   const loanRate = document.getElementById("loan-rate")
+//   return {
+//     amount: +(loanAmount.value),
+//     years: +(loanYears.value),
+//     rate: +(loanRate.value),
+//   }
+// }
+
+
+// function setupIntialValues() {
+//   const loanAmount = document.getElementById("loan-amount")
+//   const loanYears = document.getElementById("loan-years")
+//   const loanRate = document.getElementById("loan-rate")
+//   let amount = 5000
+//   let years = 5
+//   let rate = 5
+// loanAmount.value = amount
+// loanYears.value = years
+// loanRate.value = rate
+// update();
+  
+// }
+
+
+
+// function update() {
+//   const accValues = getCurrentUIValues();
+//   updateMonthly(calculateMonthlyPayment(accValues));
+// }
+
+
+// function calculateMonthlyPayment(values) {
+//   let P = loanAmount.value
+//   let i = Math.floor(loanYears.value * 12)
+//   let n = (loanRate.value / 100)  / 12
+
+//   let monthlyPayment = ((n * amount) / (1 - Math.pow((1 + n), -i))).toFixed(2)
+//   return monthlyPayment
+  
+// }
+
+// function updateMonthly(monthly) {
+//   const newBalance = document.getElementById("monthly-payment")
+//   newBalance.innerText = "$" +monthly
+// }
+
 window.addEventListener('DOMContentLoaded', function() {
   const form = document.getElementById("calc-form");
-  const loanAmount = document.getElementById("loan-amount")
-  const loanYears = document.getElementById("loan-years")
-  const loanRate = document.getElementById("loan-rate")
-  //const cal = document.getElementById("calc-submit") 
-  const newBalance = document.getElementById("monthly-payment")
-  let amount = 5000
-  let years = 5
-  let rate = 5
   if (form) {
     setupIntialValues();
     form.addEventListener("submit", function(e) {
@@ -15,46 +73,42 @@ window.addEventListener('DOMContentLoaded', function() {
       update();
     });
   }
-
-
-
-  function getCurrentUIValues() {
-    return {
-      amount: +(loanAmount.value),
-      years: +(loanYears.value),
-      rate: +(loanRate.value),
-    }
-  }
-
-
-  function setupIntialValues() {
-  
-  loanAmount.value = amount
-  loanYears.value = years
-  loanRate.value = rate
-  update();
-   
-  }
-
-
-
-  function update() {
-   const accValues = getCurrentUIValues();
-   updateMonthly(calculateMonthlyPayment(accValues));
-  }
-
-
-  function calculateMonthlyPayment(values) {
-    let P = loanAmount.value
-    let i = Math.floor(loanYears.value * 12)
-    let n = (loanRate.value / 100)  / 12
-
-    let monthlyPayment = ((n * amount) / (1 - Math.pow((1 + n), -i))).toFixed(2)
-    return monthlyPayment
-    
-  }
-
-  function updateMonthly(monthly) {
-   newBalance.innerText = "$" +monthly
-  }
 });
+
+function getCurrentUIValues() {
+  return {
+    amount: +(document.getElementById("loan-amount").value),
+    years: +(document.getElementById("loan-years").value),
+    rate: +(document.getElementById("loan-rate").value),
+  }
+}
+
+function setupIntialValues() {
+  const values  = { amount: 10000, years: 10, rate: 4.5 };
+  const amountUI = document.getElementById("loan-amount");
+  amountUI.value = values.amount;
+  const yearsUI = document.getElementById("loan-years");
+  yearsUI.value = values.years;
+  const rateUI = document.getElementById("loan-rate");
+  rateUI.value = values.rate;
+  update();
+}
+
+function update() {
+  const currentUIValues = getCurrentUIValues();
+  updateMonthly(calculateMonthlyPayment(currentUIValues));
+}
+
+function calculateMonthlyPayment(values) {
+  const monthlyRate = (values.rate / 100) / 12;
+  const n = Math.floor(values.years * 12);
+  return (
+    (monthlyRate * values.amount) /
+    (1 - Math.pow((1 + monthlyRate), -n))
+  ).toFixed(2);
+}
+
+function updateMonthly(monthly) {
+  const monthlyUI = document.getElementById("monthly-payment");
+  monthlyUI.innerText = "$" + monthly;
+}
